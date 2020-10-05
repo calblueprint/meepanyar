a8b8b4382c16d1048a87aa3123fb344a7c222cf3
Emma Judith Stephan <estephan@berkeley.edu>
2020-10-04 17:31:43 -0700

    current homescreen progress

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,21 @@ import NavBar from './components/NavBar';
 import About from './screens/About';
 import Other from './screens/Other';
 import Home from './screens/Home';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 //import NavbarContent from './components/NavBarContent';
 //import NavbarIcon from './components/NavBarIcon';
 //import Navbar from 'react-material-navbar';
 
 function App() {
   return (
-    <Router basename={}>
+    <BrowserRouter>
         <div className="App">
           <NavBar />
             <Route name="home" exact path="/" component={Home}/>
             <Route name="about" exact path="/about" component={About}/>
             <Route name="other" exact path="/other" component={Other}/>
         </div>
-      </Router>
+    </BrowserRouter>
   );
 }
 
