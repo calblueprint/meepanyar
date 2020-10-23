b9f1a1891abf170b525372ce08b83ecccce7e124
Jen Hoang <jenhoang@berkeley.edu>
2020-10-23 16:43:40 -0700

    Add Bottom Navigation (#8)
    * home screen progress
    
    * more screens
    
    * current homescreen progress
    
    * temporary navbar
    
    * improved homescreen
    
    * home screen with new features
    
    * switch to material ui library
    
    * cleanup, styled components
    
    * top nav bar with styled components
    
    * relabeled variable
    
    * separate bottom navigation PR
    
    Co-authored-by: Emma Judith Stephan <estephan@berkeley.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from './screens/User';
+import Inventory from './screens/Inventory';
+import Home from './screens/Home';
+import Incidents from './screens/Incidents';
+import Maintenance from './screens/Maintenance';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LabelBottomNavigation from './components/BottomNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <LabelBottomNavigation />
+        <Switch>
+          <Route path="/home" component={Home}/>
+          <Route path="/customers" component={User}/>
+          <Route path="/inventory" component={Inventory}/>
+          <Route path="/maintenance" component={Maintenance}/>
+          <Route path="/incidents" component={Incidents}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/styles/BottomNavStyles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+
+export const BottomNav = styled(BottomNavigation)`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-shadow: 0 0 10px 5px #e5e5e5;
+  overflow: hidden;
+`;
