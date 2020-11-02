ebb176fcc568dfd51b93864a0deec0882188b769
Kyle Hua <kyleqhua@gmail.com>
2020-11-01 16:59:40 -0800

    can switch site

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,49 +5,88 @@ import React from "react";
 import Menu from "../components/HamburgerMenu";
 import FinancialSumCard from "../components/FinancialSumCard";
 import HomeInfoCard from "../components/HomeInfo";
+import { mockComponent } from "react-dom/test-utils";
+
+interface HomeProps {
+
+}
+
+interface HomeState {
+    selectedSite: SiteData;
+    sites: Array<SiteData>;
+}
+
+interface SiteData {
+    name: string;
+    customer: string;
+    payment: string;
+    unpaid: string;
+    incidents: string;
+}
 
 let MockSiteData = [
     {
         "name": "Julians House",
-        "customers": 2,
-        "payment": 4,
-        "unpaid": 0,
-        "incidents": 1
+        "customer": "2",
+        "payment": "4",
+        "unpaid": "0",
+        "incidents": "1"
     },
     {
         "name": "Kyles House",
-        "customers": 4,
-        "payment": 2,
-        "unpaid": 1,
-        "incidents": 3
+        "customer": "4",
+        "payment": "2",
+        "unpaid": "1",
+        "incidents": "3"
     },
     {
         "name": "Jens House",
-        "customers": 1,
-        "payment": 3,
-        "unpaid": 3,
-        "incidents": 0
+        "customer": "1",
+        "payment": "3",
+        "unpaid": "3",
+        "incidents": "0"
     },
-]
-class Home extends React.Component {
+];
+
+
+class Home extends React.Component<HomeProps, HomeState> {
+
+    constructor(props: HomeProps) {
+        super(props)
+        this.state = {
+            selectedSite: MockSiteData[0],
+            sites: MockSiteData
+        }
+        this.handleSiteChange = this.handleSiteChange.bind(this);
+    }
+
+    renderSite() {
+        let selectedSite = this.state.selectedSite;
+        return (
+            <HomeInfoCard
+                customer={selectedSite.customer}
+                payment={selectedSite.payment}
+                unpaid={selectedSite.unpaid}
+                incidents={selectedSite.incidents}
+            />
+        );
+    }
+
+    handleSiteChange(newSite: SiteData) {
+        this.setState({ selectedSite: newSite });
+    }
 
     render() {
+        let siteData = this.state.sites;
         return (
             <div>
                 <FormControl >
-                    <Select
-
-                    >
-                        {MockSiteData.map(site => <MenuItem>{site.name}</MenuItem>)}
+                    <Select inputProps={{ 'aria-label': 'Without label' }}>
+                        {siteData.map((site: SiteData) => <MenuItem onClick={() => this.handleSiteChange(site)}>{site.name}</MenuItem>)}
                     </Select>
                 </FormControl>
-                <HomeInfoCard
-                    customer={"1"}
-                    payment={"1"}
-                    unpaid={"1"}
-                    incidents={"0"}
-                ></HomeInfoCard>
-                <FinancialSumCard></FinancialSumCard>
+                {this.renderSite()}
+                <FinancialSumCard />
             </div>
         )
     }
