7d9a5b2e2279c89dc51cb5991023e646bd46cf3d
mdterrenal <mariaterrenal91@gmail.com>
2020-11-01 18:10:15 -0800

    Initial creation of financial report

---
+++ src/screens/FinancialReport.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FormControl, FormHelperText, MenuItem, Select } from '@material-ui/core';
+
+class FinancialReport extends React.Component {
+  render() {
+    return <h1>This is the Financial Report</h1>;
+  }
+}
+
+export default FinancialReport;
