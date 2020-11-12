256a40a248cb0bc3a73a8ec6872c7743a40dcc83
jenmhoang <jenhoang@berkeley.edu>
2020-11-12 14:06:45 -0800

    record functionality

--- src/screens/Customers/CustomerRecords.tsx
+++ src/screens/Customers/CustomerRecords.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import BaseHeader from '../../components/BaseComponents/BaseHeader';
-import { InvoiceRecord, MeterReadingRecord } from '../../utils/airtable/interface';
+import { InvoiceRecord, PaymentRecord, MeterReadingRecord } from '../../utils/airtable/interface';
+import * as Styles from '../../styles/RecordsStyles';
+import * as BaseStyles from '../../styles/CustomerStyles';
+import Container from '@material-ui/core/Container';
+import { TabContext, TabPanel } from '@material-ui/lab';
+import Invoice from '../../components/Invoice';
+import Payment from '../../components/Payment';
 
 interface CustomerRecordsProps {
   classes: any;
@@ -10,8 +16,43 @@ interface CustomerRecordsProps {
 function CustomerRecords(props: CustomerRecordsProps) {
   const meterReadings: MeterReadingRecord[] = props.location.state.meterReadings;
   const invoices: InvoiceRecord[] = props.location.state.invoices;
+  const [value, setValue] = React.useState('0');
+  const changeTab = (event: any, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
+    <Container component="main" maxWidth="xs">
+      <BaseStyles.HeaderDiv>
+        <BaseStyles.BackButton>
+          <BaseStyles.BackArrow />
+        </BaseStyles.BackButton>
+        <Styles.HeaderText>Records</Styles.HeaderText>
+      </BaseStyles.HeaderDiv>
+      <Styles.MainDiv>
+        <TabContext value={value}>
+          <Styles.RecordAppBar position="static">
+            <Styles.RecordTabs textColor="primary" indicatorColor="primary" value={value} onChange={changeTab}>
+              <Styles.RecordTab label="Invoices" value="0" />
+              <Styles.RecordTab label="Payment" value="1" />
+            </Styles.RecordTabs>
+          </Styles.RecordAppBar>
+          <Styles.ScrollDiv>
+            <TabPanel value="0" id="invoices">
+              {invoices.map((invoice: InvoiceRecord, index) => (
+                <Invoice date={invoice.date} used_kwh={invoice.amount} amount_ks={invoice.amount} />
+              ))}
+            </TabPanel>
+            <TabPanel value="1" id="payments">
+              {meterReadings.map((reading: MeterReadingRecord, index) => (
+                <Payment date={reading.date} amount_ks={reading.reading} />
+              ))}
+            </TabPanel>
+          </Styles.ScrollDiv>
+        </TabContext>
+      </Styles.MainDiv>
+    </Container>
+    /*
     <>
       <BaseHeader leftIcon="backNav" />
       <h3>Meter Readings</h3>
@@ -23,7 +64,7 @@ function CustomerRecords(props: CustomerRecordsProps) {
       {invoices.map((invoice: InvoiceRecord, index) => (
         <p key={invoice.rid}>{`${invoice.date} ${invoice.amount}`}</p>
       ))}
-    </>
+    </>*/
   );
 }
 
