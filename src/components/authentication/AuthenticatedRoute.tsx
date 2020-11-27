8a7d5172b6f2e5475ada1aa9f19abcbd3b281d30
julianrkung <julianrkung@berkeley.edu>
2020-11-27 15:42:40 -0800

    Sign in Authentication (#17)
    Added authenticated routes

---
+++ src/components/authentication/AuthenticatedRoute.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { RootState } from '../../lib/redux/store';
+import { connect } from 'react-redux';
+import LoadingComponent from '../BaseComponents/LoadingComponent';
+
+interface AuthenticatedRouteProps {
+  isLoading: boolean;
+  isSignedIn: boolean;
+  path: string;
+  component: React.ComponentType<any>;
+  exact?: boolean;
+}
+
+const AuthenticatedRoute = ({ isLoading, isSignedIn, path, component, exact }: AuthenticatedRouteProps) => {
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+
+  return (
+    <Route
+      render={() => {
+        return isSignedIn ? <Route component={component} path={path} exact={exact} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+const mapStateToProps = (state: RootState) => ({
+  isLoading: state.userData.isLoading,
+  isSignedIn: state.userData.user !== null,
+});
+
+export default connect(mapStateToProps)(AuthenticatedRoute);
