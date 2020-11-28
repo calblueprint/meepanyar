0c4f519488c5657233c7a7df5892816fa173c0b6
jenmhoang <jenhoang@berkeley.edu>
2020-11-28 15:49:38 -0800

    transition to createStyles

--- src/components/BaseComponents/BottomNav.tsx
+++ src/components/BaseComponents/BottomNav.tsx
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { BottomNav } from '../../styles/BottomNavStyles';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
 import BuildIcon from '@material-ui/icons/Build';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import ReportProblemIcon from '@material-ui/icons/ReportProblem';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      boxShadow: '0 0 10px 5px #e5e5e5',
+      overflow: 'hidden',
+    },
+  });
+
+// const styles = (theme: Theme) =>
+//   createStyles({
+//     root: {
+//       position: 'absolute',
+//       bottom: 0,
+//       width: '100%',
+//       box-shadow: '0 0 10px 5px #e5e5e5'
+//       overflow: 'hidden',
+//     },
+//   },
+// });
 
 interface NavState {
   selectedScreen: string;
 }
 
-export default class LabelBottomNavigation extends React.Component<{}, NavState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      selectedScreen: 'recents',
-    };
-  }
+function BaseNavigation(props: {}) {
+  const [selectedScreen, setSelectedScreen] = useState('recents');
 
-  changeNavOption = (event: React.ChangeEvent<{}>, newSelection: string) => {
-    this.setState({
-      selectedScreen: newSelection,
-    });
+  const changeNavOption = (event: React.ChangeEvent<{}>, newSelection: string) => {
+    setSelectedScreen(newSelection);
   };
 
-  render() {
-    return (
-      <BottomNav value={this.state.selectedScreen} onChange={this.changeNavOption}>
-        <BottomNavigationAction value="/home" icon={<HomeIcon />} component={Link} to="/home" />
-        <BottomNavigationAction value="/customers" icon={<PersonIcon />} component={Link} to="/customers" />
-        <BottomNavigationAction value="/inventory" icon={<ShoppingCartIcon />} component={Link} to="/inventory" />
-        <BottomNavigationAction value="/maintenance" icon={<BuildIcon />} component={Link} to="/maintenance" />
-        <BottomNavigationAction value="/incidents" icon={<ReportProblemIcon />} component={Link} to="/incidents" />
-      </BottomNav>
-    );
-  }
+  return (
+    <BottomNavigation value={selectedScreen} onChange={changeNavOption}>
+      <BottomNavigationAction value="/home" icon={<HomeIcon />} component={Link} to="/home" />
+      <BottomNavigationAction value="/customers" icon={<PersonIcon />} component={Link} to="/customers" />
+      <BottomNavigationAction value="/inventory" icon={<ShoppingCartIcon />} component={Link} to="/inventory" />
+      <BottomNavigationAction value="/maintenance" icon={<BuildIcon />} component={Link} to="/maintenance" />
+      <BottomNavigationAction value="/incidents" icon={<ReportProblemIcon />} component={Link} to="/incidents" />
+    </BottomNavigation>
+  );
 }
+
+export default withStyles(styles)(BaseNavigation);
