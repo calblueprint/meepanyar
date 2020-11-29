5cac169446080a4f2e14b027822626ff1e905445
julianrkung <julianrkung@berkeley.edu>
2020-11-28 16:38:45 -0800

    Add Site Data to redux (#18)
    Add site data, customer information, and meter readings and payments to redux.
    Change router to use ConnectedRouter so http requests arent aborted

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,12 +1,21 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import logger from 'redux-logger';
+import { createBrowserHistory } from 'history';
 import thunk from 'redux-thunk';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 
 import userDataSlice from './userDataSlice';
+import siteDataSlice from './siteDataSlice';
 
-const allReducers = combineReducers({ userData: userDataSlice });
+const history = createBrowserHistory();
+
+const allReducers = combineReducers({
+  userData: userDataSlice,
+  siteData: siteDataSlice,
+  router: connectRouter(history),
+});
 
 const persistConfig = {
   key: 'root',
@@ -17,10 +26,10 @@ const persistedReducer = persistReducer(persistConfig, allReducers);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: [thunk, logger],
+  middleware: [thunk, logger, routerMiddleware(history)],
 });
 
 const persistor = persistStore(store);
 
-export { store, persistor };
+export { history, store, persistor };
 export type RootState = ReturnType<typeof store.getState>;
