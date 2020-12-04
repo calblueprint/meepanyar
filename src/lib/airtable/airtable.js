4cc163986fa87a1f93d149b702d27c089f359bae
Julian Kung <julianrkung@berkeley.edu>
2020-12-03 17:47:41 -0800

    new library WIP

--- src/lib/airtable/airtable.js
+++ src/lib/airtable/airtable.js
@@ -1,5 +1,5 @@
 import Airtable from '@calblueprint/airlock';
-import { opendb } from 'idb';
+import { openDB } from 'idb';
 
 const BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
 const API_KEY = 'airlock';
@@ -13,59 +13,94 @@ Airtable.configure({
 const base = Airtable.base(BASE_ID);
 
 const addToOfflineCustomer = async (customer, fieldToAppend, objectToAdd) => {
-    const db = await opendb
+    const db = await openDB('workbox-background-sync');
+    const transaction = db.transaction('requests', 'readwrite');
+    const objectStore = transaction.objectStore('requests');
+    let requestCursor = await objectStore.openCursor();
+    const requestKeyToValue = {};
+    const requestKeyToBody = {};
 
-    const dbRequest = createDBRequest('workbox-background-sync');
+    console.log(customer, fieldToAppend, objectToAdd);
 
-    dbRequest.onsuccess = event => {
-        const dbInstance = event.target.result;
-        const transactionInstance = createTransaction(dbInstance, ["requests"]);
-        const objectStore = openObjectStore(transactionInstance, "requests");
+    // In this transaction, iterate through all the requests and store them in the requestKeyValues
+    while (requestCursor) {
+        try {
+            const requestValue = requestCursor.value;
+            const requestKey = requestCursor.key;
+            console.log(requestKey, requestValue);
 
-        objectStore.openCursor().onsuccess = event => {
-            const cursor = event.target.result;
-
-            if (cursor) {
-                try {
-                    console.log("Cursor key: ", cursor.key);
-                    console.log("Cursor value: ", cursor.value);
-                    const cursorValue = { ...cursor.value };
-                    const requestBodyBlob = cursorValue.storableRequest.requestInit.body;
-
-                    getJSONFromBlob(requestBodyBlob).then(bodyObject => {
-                        console.log("Body object ", bodyObject);
-
-                    })
-
-                } catch (error) {
-                    console.log("Error occurred when iterating through cursor ", error);
-                    cursor.continue();
-                }
-
-                const newBody = JSON.stringify({
-                    ...customer,
-                    [fieldToAppend]: [objectToAdd]
-                })
-                const newBlob = new Blob([newBody], { type: 'application/json' })
-                cursorValue.storableRequest.requestInit.body = newBlob;
-                console.log("Changed value: ", cursorValue);
-
-                const updateRequest = cursor.update(cursorValue);
-                updateRequest.onsucces = event => {
-                    console.log("Cursor update success");
-                };
-                updateRequest.onerror = event => {
-                    console.log("Cursor update error", event);
-                }
-
-                cursor.continue();
-            } else {
-                console.log("Cursor finished iterating");
+            if (requestValue && requestValue.storableRequest && requestValue.storableRequest.requestInit) {
+                const requestBlob = requestValue.storableRequest.requestInit.body;
+                requestKeyToValue[requestKey] = requestBlob;
             }
+        } catch (error) {
+            console.log('Error when iterating through cursor on key ', requestCursor.key);
         }
+        requestCursor = await requestCursor.continue();
     }
-}
+    await transaction.done;
 
+    // Decode the requestBlobs
+    for (const [requestKey, requestValue] of Object.entries(requestKeyToValue)) {
+        const requestBodyBlob = requestValue.storableRequest.requestInit.body;
+        const requestBody = await requestBodyBlob.text();
+        const requestBodyJSON = JSON.parse(requestBody);
+        requestKeyToBody[requestKey] = requestBodyJSON;
+    }
+
+    // Decode the requestBlobs
+
+
+    // const dbRequest = createDBRequest('workbox-background-sync');
+
+    // dbRequest.onsuccess = event => {
+    //     const dbInstance = event.target.result;
+    //     const transactionInstance = createTransaction(dbInstance, ["requests"]);
+    //     const objectStore = openObjectStore(transactionInstance, "requests");
+
+    //     objectStore.openCursor().onsuccess = event => {
+    //         const cursor = event.target.result;
+
+    //         if (cursor) {
+    //             try {
+    //                 console.log("Cursor key: ", cursor.key);
+    //                 console.log("Cursor value: ", cursor.value);
+    //                 const cursorValue = { ...cursor.value };
+    //                 const requestBodyBlob = cursorValue.storableRequest.requestInit.body;
+
+    //                 getJSONFromBlob(requestBodyBlob).then(bodyObject => {
+    //                     console.log("Body object ", bodyObject);
+
+    //                 })
+
+    //             } catch (error) {
+    //                 console.log("Error occurred when iterating through cursor ", error);
+    //                 cursor.continue();
+    //             }
+
+    //             const newBody = JSON.stringify({
+    //                 ...customer,
+    //                 [fieldToAppend]: [objectToAdd]
+    //             })
+    //             const newBlob = new Blob([newBody], { type: 'application/json' })
+    //             cursorValue.storableRequest.requestInit.body = newBlob;
+    //             console.log("Changed value: ", cursorValue);
+
+    //             const updateRequest = cursor.update(cursorValue);
+    //             updateRequest.onsucces = event => {
+    //                 console.log("Cursor update success");
+    //             };
+    //             updateRequest.onerror = event => {
+    //                 console.log("Cursor update error", event);
+    //             }
+
+    //             cursor.continue();
+    //         } else {
+    //             console.log("Cursor finished iterating");
+    //         }
+    //     }
+    // }
+}
 
 const createCustomer = async (customer) => {
     try {
