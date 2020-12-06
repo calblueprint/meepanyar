ef3c583bd43c9ab520fc09464948809ee2063a25
mdterrenal <35015640+mdterrenal@users.noreply.github.com>
2020-12-05 22:26:55 -0800

    Financial Summary Screens (#14)
    * Initial creation of financial report
    
    * Add in content/structure for financial summary
    
    * Formatting changes
    
    * One last formatting change
    
    * Unstash previous work
    
    * Formatting changes
    
    * Complete styling
    
    * Formatting changes
    
    * Pull data from Airtable and some refactoring
    
    * Formatting changes yet again
    
    * Readd generate schema script
    
    * Forgot to save my file
    
    * Finish up financial summary screens
    
    * Linting and all that
    
    * Initial changes to address some comments
    
    * Formatting changes
    
    * Replace index with label for key
    
    * Address more comments in pr
    
    * Eslint
    
    * Initial progress with creating objects
    
    * Eslint
    
    * Remove some changes for now
    
    * Forgot last merge conflict
    
    * Eslint
    
    * Reverting changes for testing
    
    * Change financial summary to authenticated route
    
    Co-authored-by: jenmhoang <jenhoang@berkeley.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import './App.css';
+import { theme } from './styles/ThemeStyles';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
+
+import LabelBottomNavigation from './components/BaseComponents/BottomNav';
 import Login from './screens/Login';
+import Home from './screens/Home';
+
 import CustomerMain from './screens/Customers/CustomerMain';
 import CustomerProfile from './screens/Customers/CustomerProfile';
 import CustomerRecords from './screens/Customers/CustomerRecords';
 import AddCustomer from './screens/Customers/AddCustomer';
 import EditCustomer from './screens/Customers/EditCustomer';
 import AddMeterReading from './screens/Customers/AddMeterReading';
+
 import Inventory from './screens/Inventory';
 import Incidents from './screens/Incidents';
 import Maintenance from './screens/Maintenance';
+
+import FinancialSummary from './screens/FinancialSummary/FinancialSummary';
+
 import { ConnectedRouter } from 'connected-react-router';
+import AuthenticatedRoute from './components/authentication/AuthenticatedRoute';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { RootState } from './lib/redux/store';
-import LabelBottomNavigation from './components/BaseComponents/BottomNav';
-import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
-import { theme } from './styles/ThemeStyles';
 import { history } from './lib/redux/store';
-import AuthenticatedRoute from './components/authentication/AuthenticatedRoute';
-import Home from './screens/Home';
 
 interface AppProps {
   isSignedIn: boolean;
@@ -40,15 +46,19 @@ function App(isSignedIn: AppProps) {
               </Route>
               <Route path="/login" component={Login} />
               <AuthenticatedRoute path="/home" component={Home} />
+
               <AuthenticatedRoute path="/customers" component={CustomerMain} exact />
+              <AuthenticatedRoute path="/customers/add" component={AddCustomer} />
+              <AuthenticatedRoute path={'/customers/:rid'} component={CustomerProfile} exact />
+              <AuthenticatedRoute path={'/customers/:rid/edit'} component={EditCustomer} />
+              <AuthenticatedRoute path="/customers/:rid/addMeter" component={AddMeterReading} />
+              <AuthenticatedRoute path={'/customers/:rid/records'} component={CustomerRecords} />
+
               <AuthenticatedRoute path="/inventory" component={Inventory} />
               <AuthenticatedRoute path="/maintenance" component={Maintenance} />
               <AuthenticatedRoute path="/incidents" component={Incidents} />
-              <AuthenticatedRoute path={'/customers/:rid'} component={CustomerProfile} exact />
-              <AuthenticatedRoute path={'/customers/:rid/records'} component={CustomerRecords} />
-              <AuthenticatedRoute path="/customers/add" component={AddCustomer} />
-              <AuthenticatedRoute path="/customers/:rid/addMeter" component={AddMeterReading} />
-              <AuthenticatedRoute path={'/customers/:rid/edit'} component={EditCustomer} />
+
+              <AuthenticatedRoute path="/financialsummary" component={FinancialSummary} />
             </Switch>
           </ConnectedRouter>
         </ThemeProvider>
---
+++ src/screens/FinancialSummary/ConfirmDialogContents.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextWrapper from '../../components/TextWrapper';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    buttons: {
+      display: 'flex',
+      justifyContent: 'space-around',
+    },
+    cancelButton: {
+      borderRadius: '18px',
+      borderWidth: '2px',
+      borderColor: theme.palette.grey[300],
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.primary.main,
+      letterSpacing: '0.14em',
+      height: '41px',
+    },
+    confirmButton: {
+      borderColor: theme.palette.primary.main,
+      borderRadius: '18px',
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+      letterSpacing: '0.14em',
+      height: '41px',
+    },
+    confirmText: {
+      margin: '24px 31px',
+      textAlign: 'center',
+      color: theme.palette.text.primary,
+      lineHeight: '120.5%',
+    },
+    dialogContents: {
+      color: theme.palette.text.primary,
+      boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
+      padding: '25px 19px',
+    },
+    valueWrappers: {
+      border: '1px solid',
+      borderColor: theme.palette.divider,
+      borderRadius: '6px',
+      padding: '22px 22px',
+    },
+    bottomMargin: {
+      marginBottom: '8px',
+    },
+  }),
+);
+
+interface ConfirmDialogContentsProps {
+  onClick: () => void;
+  profitNumbers: number[];
+}
+
+export default function ConfirmDialogContents(props: ConfirmDialogContentsProps): JSX.Element {
+  const classes = useStyles();
+
+  const profitLabels = ['Total Profit', 'Your Profit', "Mee Panyar's Profit"];
+  const currencyUnits = [' Ks', ' Ks', ' Ks'];
+  const zeroes = [0, 0];
+
+  return (
+    <div className={classes.dialogContents}>
+      <div className={classes.valueWrappers}>
+        <TextWrapper
+          labels={profitLabels.slice(0, 1)}
+          numbers={props.profitNumbers}
+          units={currencyUnits.slice(0, 1)}
+          bold
+          styling={classes.bottomMargin}
+        />
+        <TextWrapper
+          labels={profitLabels.slice(1)}
+          numbers={zeroes}
+          units={currencyUnits.slice(1)}
+          color="textSecondary"
+          bold
+        />
+      </div>
+      <Typography variant="body1" className={classes.confirmText}>
+        Please make sure that you want to close the current period before clicking "Confirm"
+      </Typography>
+      <div className={classes.buttons}>
+        <Button className={classes.cancelButton} size="medium" onClick={props.onClick} variant="contained">
+          <Typography variant="body2">Cancel</Typography>
+        </Button>
+        <Button className={classes.confirmButton} size="medium" variant="contained">
+          <Typography variant="body2">Confirm</Typography>
+        </Button>
+      </div>
+    </div>
+  );
+}
