32a27a0122ad649ad08f6a5e1459702a939b6afa
Julian Kung <julianrkung@berkeley.edu>
2020-12-09 21:20:15 -0800

    Removed demo from home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,123 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../lib/redux/store';
 import { formatUTCDateStringToLocal } from '../lib/moment/momentUtils';
-import {
-  getAllSites,
-  getAllTariffPlans,
-  createCustomer as createCustomers,
-  createMeterReadingsandInvoice,
-} from '../utils/new-schema/request';
-import moment from 'moment';
-import { getUserId } from '../lib/redux/userData';
 
 interface HomeProps {
   lastUpdated: string;
   isOnline: boolean;
 }
 
-interface HomeState {
-  sites: any[];
-  tariffPlans: any[];
-}
-
-class Home extends React.Component<HomeProps, HomeState> {
-  state = {
-    sites: [],
-    tariffPlans: [],
-  };
-
-  public async componentDidMount() {
-    await this.getSiteData();
-    await this.getTariffPlans();
-  }
-
-  getCustomerName = () => {
-    return 'Julian Kung';
-  };
-
-  getSite = () => {
-    return 'recdokHLQX8zCHofF';
-  };
-
-  getTariffPlan = () => {
-    return 'recoeD24hlQuSPCKQ';
-  };
-
-  getMeterNumber = () => {
-    return 2323;
-  };
-
-  createCustomer = async (): Promise<void> => {
-    const customerName = this.getCustomerName();
-    const siteId = this.getSite();
-    const meterNumber = this.getMeterNumber();
-    const tariffPlanId = this.getTariffPlan();
-
-    const customerData = {
-      name: customerName,
-      meterNumber: meterNumber,
-      tariffPlansId: tariffPlanId,
-      sitesId: siteId,
-      meterReadings: [],
-      payments: [],
-    };
-
-    createCustomers(customerData);
-  };
-
-  createMeterReading = async (): Promise<void> => {
-    const customerName = this.getCustomerName();
-    const siteId = this.getSite();
-    const meterNumber = this.getMeterNumber();
-    const tariffPlanId = this.getTariffPlan();
-
-    const customerData = {
-      name: customerName,
-      meterNumber: meterNumber,
-      tariffPlansId: tariffPlanId,
-      sitesId: siteId,
-      meterReadings: [],
-      payments: [],
-    };
-
-    const meterReading = {
-      reading: 10,
-      meterNumber: 10,
-      date: moment().format('YYYY-MM-DD'),
-      amountBilled: 100,
-      billedById: getUserId(),
-    };
-    createMeterReadingsandInvoice(meterReading, customerData);
-  };
-
-  getTariffPlans = async () => {
-    const allTariffPlans = await getAllTariffPlans();
-    console.log(allTariffPlans);
-
-    this.setState({ tariffPlans: allTariffPlans });
-  };
-
-  getSiteData = async () => {
-    const allSiteData = await getAllSites();
-    console.log(allSiteData);
-
-    this.setState({ sites: allSiteData });
-  };
-
+class Home extends React.Component<HomeProps, {}> {
   render() {
-    const { sites, tariffPlans } = this.state;
     return (
-      <>
-        <button onClick={this.createCustomer}>Create a customer</button>
-        <button onClick={this.createMeterReading}>Create a Meter Reading for customer</button>
-        <h4>
-          {JSON.stringify(tariffPlans)}
-          This is the home screen
-          <p> User data last updated on: {this.props.lastUpdated}</p>
-          {!this.props.isOnline ? <p>Offline</p> : null}
-          {JSON.stringify(sites)}
-        </h4>
-      </>
+      <h4>
+        This is the home screen
+        <p> User data last updated on: {this.props.lastUpdated}</p>
+        {!this.props.isOnline ? <p>Offline</p> : null}
+      </h4>
     );
   }
 }
@@ -128,13 +25,11 @@ const mapStateToProps = (state: RootState) => {
   let isOnline = false;
 
   if (state.userData) {
-    const userData = state.userData;
-
-    if (userData.isOnline) {
-      isOnline = userData.isOnline;
+    if (state.userData.isOnline) {
+      isOnline = state.userData.isOnline;
     }
 
-    if (userData.lastUpdated) {
+    if (state.userData.lastUpdated) {
       lastUpdated = formatUTCDateStringToLocal(state.userData.lastUpdated);
     }
   }
