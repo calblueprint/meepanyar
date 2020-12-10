79e0471ee0503c9dcbbd28ef4673c98fa7f7ae10
jenmhoang <jenhoang@berkeley.edu>
2020-12-09 19:40:55 -0800

    pr fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
