6027714f4d7233c431b1f134dc64c3a78e70034a
Julian Kung <julianrkung@berkeley.edu>
2020-12-28 12:32:57 -0800

    Moved additional API calls to backend. Removed user from param

--- src/lib/redux/siteData.ts
+++ src/lib/redux/siteData.ts
@@ -1,31 +1,13 @@
 import { store } from './store';
 import { setLoadingForSiteData, setCurrSite, saveSiteData } from './siteDataSlice';
-import { getAllSites, getCustomersByIds, getMeterReadingsandInvoicesByIds, getPaymentsByIds } from '../airtable/request';
-import { CustomerRecord, MeterReadingRecord, PaymentRecord } from '../airtable/interface';
+import { getAllSites } from '../airtable/request';
 
-const refreshSiteData = async (user: any): Promise<void> => {
+const refreshSiteData = async (): Promise<void> => {
   store.dispatch(setLoadingForSiteData());
   let currentSite = null;
+
+  // Sites authentication is done on backend
   const sites = await getAllSites();
-
-  for (const site of sites) {
-    const customerIds = site.customerIds;
-    let customers: CustomerRecord[] = [];
-
-    if (customerIds) {
-      customers = await getCustomersByIds(customerIds);
-
-      // Grab payments and meter readings for each customer
-      for (const customer of customers) {
-        const meterReadings = await getMeterReadingsForCustomer(customer);
-        const payments = await getPaymentsForCustomer(customer);
-        customer.meterReadings = meterReadings;
-        customer.payments = payments;
-      }
-    }
-
-    site.customers = customers;
-  }
 
   if (sites.length > 0) {
     currentSite = sites[0];
@@ -41,29 +23,6 @@ const refreshSiteData = async (user: any): Promise<void> => {
 
 const setCurrentSite = (newSite: any): void => {
   store.dispatch(setCurrSite(newSite));
-};
-
-// Helper function to grab a meter reading for a specific customer
-const getMeterReadingsForCustomer = async (customer: CustomerRecord): Promise<MeterReadingRecord[]> => {
-  const meterReadingIds = customer.meterReadingIds;
-  let meterReadings: MeterReadingRecord[] = [];
-
-  if (meterReadingIds) {
-    meterReadings = await getMeterReadingsandInvoicesByIds(meterReadingIds);
-  }
-  return meterReadings;
-};
-
-// Helper function to grab a payment for a specific customer
-const getPaymentsForCustomer = async (customer: CustomerRecord): Promise<PaymentRecord[]> => {
-  const paymentIds = customer.paymentIds;
-  let payments: PaymentRecord[] = [];
-
-  if (paymentIds) {
-    payments = await getPaymentsByIds(paymentIds);
-  }
-
-  return payments;
 };
 
 export { refreshSiteData, setCurrentSite };
