9e01dfa4c354e55aa8423a0fa1e5d1f15f67de0b
cnnmon <wangttiffany@gmail.com>
2021-02-22 19:29:36 -0800

    installing packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,74 @@
 import React from 'react';
-//import logo from './logo.svg';
-import './App.css';
-// import Navigation from './components/Navigation';
-import User from './screens/User';
-import Shopping from './screens/Shopping';
-import Home from './screens/Home';
-import Errors from './screens/Errors';
-import Tools from './screens/Tools';
+import { theme } from './styles/ThemeStyles';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import BaseNavigation from './components/BaseComponents/BaseNavigation';
+import Login from './screens/Login';
+import Home from './screens/Home/Home';
+
+import CustomerMain from './screens/Customers/CustomerMain';
+import CustomerProfile from './screens/Customers/CustomerProfile';
+import CustomerRecords from './screens/Customers/CustomerRecords';
+import AddCustomer from './screens/Customers/AddCustomer';
+import EditCustomer from './screens/Customers/EditCustomer';
+import AddMeterReading from './screens/Customers/AddMeterReading';
 import SubmittedRecords from './screens/SubmittedRecords';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SearchNavBar from './components/SearchNavBar';
-import LabelBottomNavigation from './components/BottomNav';
+
+import Inventory from './screens/Inventory';
+import Incidents from './screens/Incidents';
+import Maintenance from './screens/Maintenance';
+
+import FinancialSummary from './screens/FinancialSummary/FinancialSummary';
+
+import { ConnectedRouter } from 'connected-react-router';
+import AuthenticatedRoute from './components/authentication/AuthenticatedRoute';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { RootState } from './lib/redux/store';
+import { connect } from 'react-redux';
+import { history } from './lib/redux/store';
+
+interface AppProps {
+  isSignedIn: boolean;
+}
+
+function App(isSignedIn: AppProps) {
+  const homeRedirect = isSignedIn ? '/home' : '/login';
+
+  const Container = () => (
+    <>
+      <AuthenticatedRoute path="/home" component={Home} />
+
+      <AuthenticatedRoute path="/customers" component={CustomerMain} exact />
+      <AuthenticatedRoute path="/customers/create" component={AddCustomer} exact />
+      <AuthenticatedRoute path={'/customers/customer'} component={CustomerProfile} exact />
+      <AuthenticatedRoute path={'/customers/customer/edit'} component={EditCustomer} exact />
+      <AuthenticatedRoute path="/customers/customer/meter-readings/create" component={AddMeterReading} exact />
+      <AuthenticatedRoute path={'/customers/customer/records'} component={CustomerRecords} exact />
+      <AuthenticatedRoute path="/records" component={SubmittedRecords} />
+
+      <AuthenticatedRoute path="/inventory" component={Inventory} />
+      <AuthenticatedRoute path="/maintenance" component={Maintenance} />
+      <AuthenticatedRoute path="/incidents" component={Incidents} />
+
+      <AuthenticatedRoute path="/financial-summary" component={FinancialSummary} exact />
+      <BaseNavigation />
+    </>
+  );
 
   return (
     <div className="App">
-      <SearchNavBar />
-      <BrowserRouter>
-      <LabelBottomNavigation />
-        <Switch>
-          <Route path="/home" component={Home}/>
-          <Route path="/customers" component={User}/>
-          <Route path="/inventory" component={Shopping}/>
-          <Route path="/maintenance" component={Tools}/>
-          <Route path="/incidents" component={Errors}/>
-          <Route path="/records" component={SubmittedRecords} />
-        </Switch>
-      </BrowserRouter>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to={homeRedirect} />
+              </Route>
+              <Route exact path="/(login)" component={Login} />
+              <Route component={Container} />
+            </Switch>
+          </ConnectedRouter>
+        </ThemeProvider>
+      </StylesProvider>
     </div>
   );
 }
