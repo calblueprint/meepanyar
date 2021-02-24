8877da36dccbf783c0e6398569aff12a44f1d586
Kyle Hua <kyleqhua@gmail.com>
2021-02-24 15:03:26 -0800

    calculations work

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { RootState } from '../../lib/redux/store';
+import { RootState, store } from '../../lib/redux/store';
 import { formatUTCDateStringToLocal } from '../../lib/moment/momentUtils';
 import { withStyles, createStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
-import { SiteRecord } from '../../lib/airtable/interface';
+import { CustomerRecord, SiteRecord } from '../../lib/airtable/interface';
 import HomeMenuItem from './components/HomeMenuItem';
 import SiteMenu from './components/SiteMenu';
 
@@ -13,6 +13,7 @@ import Typography from '@material-ui/core/Typography';
 import WifiIcon from '@material-ui/icons/Wifi';
 import WifiOffIcon from '@material-ui/icons/WifiOff';
 import BaseScreen from '../../components/BaseComponents/BaseScreen';
+import { getAllSites } from '../../lib/airtable/request';
 
 const styles = () =>
   createStyles({
@@ -37,6 +38,70 @@ interface HomeProps {
 
 function Home(props: HomeProps) {
   const { classes, lastUpdated, isOnline, currentSite } = props;
+  useEffect(() => {
+    proccessData();
+  }, []);
+
+  const [outStandingPayments, setOutstandingPayments] = useState<number>();
+  const [toCharge, setToCharges] = useState<number>();
+  const [customerDetails, setCustomerDetails] = useState<number>();
+  const [unpaidReports, setUnpaidReports] = useState<number>();
+
+
+
+  const proccessData = () => {
+    calculateCustomerData(currentSite);
+    calculateUnpaidReports(currentSite)
+  }
+
+  const calculateCustomerData = (ss: SiteRecord) => {
+    // customers to charge:  haven't charged yet/ no meterreading
+    // outstanding payments: done the meter reading / charged, haven't paid yet
+    let toCharge = 0;
+    let outstanding = 0;
+    //get day
+    const today = new Date();
+    const currMonth = today.getUTCMonth();
+    let customers = ss.customers;
+    for (let i = 0; i < customers.length; i++) {
+      let curr = customers[i]
+      //depends if the meter readings list is sorted with earliest => latest
+      let latestMeterReading = curr.meterReadings[curr.meterReadings.length - 1];
+      if (latestMeterReading != null) {
+        const dateTime = Date.parse(latestMeterReading.date);
+        let readingDate = new Date(dateTime);
+        if (readingDate.getUTCMonth() < currMonth) {
+          toCharge += 1;
+        }
+        if (checkPayment(curr)) {
+          outstanding += 1;
+        }
+      }
+    }
+    setOutstandingPayments(outstanding);
+    setToCharges(toCharge);
+    setCustomerDetails(toCharge + outstanding);
+  }
+
+  //true has outstadining false has paid
+  const checkPayment = (customer: CustomerRecord) => {
+    if (parseInt(customer.outstandingBalance) > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  const calculateUnpaidReports = (ss: SiteRecord) => {
+    let unpaid = 0;
+    let summaries = ss.financialSummaries;
+    for (let i = 0; i < summaries.length; i++) {
+      let singleSummary = summaries[i];
+      if (singleSummary.totalCustomersBilled > singleSummary.totalCustomersPaid) {
+        unpaid += 1;
+      }
+    }
+    setUnpaidReports(unpaid);
+  }
 
   return (
     <BaseScreen rightIcon="user">
@@ -54,7 +119,7 @@ function Home(props: HomeProps) {
       <Link to={'/customers'}>
         <HomeMenuItem
           label="Customer Alerts"
-          amount={1}
+          amount={customerDetails ? customerDetails : 0}
           sublabels={[
             { amount: 1, label: 'Customers to Charge' },
             { amount: 2, label: 'Outstanding Payments' },
@@ -62,7 +127,7 @@ function Home(props: HomeProps) {
         />
       </Link>
       <Link to={'/financial-summary'}>
-        <HomeMenuItem label="Unpaid Reports" amount={1} />
+        <HomeMenuItem label="Unpaid Reports" amount={unpaidReports} />
       </Link>
       <HomeMenuItem label="Unresolved Incidents" amount={0} />
       <Link to={'/financial-summary'}>
