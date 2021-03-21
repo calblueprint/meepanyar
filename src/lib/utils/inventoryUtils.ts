4c7d1010b3a9da15edc8d852ba679b6cced0460b
Annie Wang <anniedwang02@gmail.com>
2021-03-21 14:29:27 -0700

    Remove unused utils

--- src/lib/utils/inventoryUtils.ts
+++
@@ -1,6 +0,0 @@
-import { ProductRecord } from "../airtable/interface";
-
-// Look up product info by product id
-export const getProductInfo = (productId: string, products: ProductRecord[]): ProductRecord | undefined  => {
-    return products.find(x => x.id === productId);
-}
