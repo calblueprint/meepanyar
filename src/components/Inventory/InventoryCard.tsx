74ba2ffed77ece83b4bc57e5a7717e6f65d065bc
Annie Wang <anniedwang02@gmail.com>
2021-03-21 14:23:33 -0700

    Styling cleanup

--- src/components/Inventory/InventoryCard.tsx
+++ src/components/Inventory/InventoryCard.tsx
@@ -10,89 +10,29 @@ import { RootState } from '../../lib/redux/store';
 const styles = (theme: Theme) =>
   createStyles({
     arrow: {
-      color: theme.palette.primary.main,
+      color: theme.palette.text.primary,
     },
     cardContent: {
-      flexGrow: 1,
-      display: 'flex',
-      margin: '0px',
-      paddingTop: '16px',
-      paddingLeft: '16px',
-    },
-    titleText: {
-      fontFamily: 'Helvetica Neue',
-      fontStyle: 'normal',
-      fontWeight: 'bold',
-      fontSize: '20px',
-      lineHeight: '98.1%',
-      color: theme.palette.text.primary,
-      paddingTop: '14px'
-    },
-    updatedText: {
-      fontFamily: 'Helvetica Neue',
-      fontStyle: 'normal',
-      fontWeight: theme.typography.body1.fontWeight,
-      fontSize: theme.typography.body1.fontSize,
-      color: '#BDBDBD',
-    },
-    totalText: {
-      fontFamily: 'Helvetica Neue',
-      fontStyle: 'normal',
-      fontWeight: theme.typography.body1.fontWeight,
-      fontSize: theme.typography.body1.fontSize,
-      color: '#6A6A6A',
-      paddingTop: '14px'
-    },
-    numberText: {
-      fontFamily: 'Helvetica Neue',
-      fontStyle: 'normal',
-      fontWeight: 'bold',
-      fontSize: '15px',
-      color: '#FF866F',
-    },
-    divSpacing: {
-      paddingLeft: '15px',
+      flex: 1,
+      padding: 16,
     },
     singleCard: {
-      width: '100%',
-      height: '93.26px',
-      background: '#ffffff',
-      border: '1px solid #e5e5e5',
-      boxSizing: 'border-box',
-      boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.14)',
-      borderRadius: '10px',
-      textAlign: 'left',
+      border: `1px solid ${theme.palette.divider}`,
+      boxShadow: 'none',
+      borderRadius: 10,
       display: 'flex',
-      flexDirection: 'row',
-      marginBottom: '10px',
-    },
-    active: {
-      height: '7px',
-      width: '7px',
-      borderRadius: '50%',
-      backgroundColor: '#64B676',
-      marginTop: '26px',
-      marginRight: '5px',
-    },
-    notActive: {
-      height: '7px',
-      width: '7px',
-      borderRadius: '50%',
-      backgroundColor: '#E0E0E0',
-      marginTop: '26px',
-      marginRight: '5px',
+      marginBottom: 10
     },
     arrowSpacing: {
-      padding: '0px',
+      padding: 0
     },
   });
 interface InventoryCardProps {
   inventoryItem: InventoryRecord;
   classes: {
-    arrow: string; cardContent: string; updatedText: string;
-    singleCard: string; titleText: string; totalText: string;
-    numberText: string; divSpacing: string; active: string;
-    notActive: string; arrowSpacing: string;
+    arrow: string; cardContent: string;
+    singleCard: string;
+    arrowSpacing: string;
   };
   products: Record<string, ProductRecord>;
 }
@@ -103,13 +43,13 @@ function InventoryCard(props: InventoryCardProps) {
     <Card className={classes.singleCard}>
       <div className={classes.cardContent}>
         <div>
-          <Typography className={classes.titleText}>{products[inventoryItem.productId]?.name}</Typography>
-          <Typography className={classes.updatedText}>Last Updated: 00.00</Typography>
+          <Typography variant="body1">{products[inventoryItem.productId]?.name}</Typography>
+          <Typography variant="body1" color="textSecondary">Last Updated: 00.00</Typography>
         </div>
       </div>
-      <CardActions className={classes.arrowSpacing}>
-        <IconButton>
-          <ArrowForwardIosIcon className={classes.arrow} />
+      <CardActions>
+        <IconButton size="small">
+          <ArrowForwardIosIcon className={classes.arrow} fontSize="small"/>
         </IconButton>
       </CardActions>
     </Card>
