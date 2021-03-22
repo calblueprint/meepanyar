8c5bf18f3fcdcb18c18c819e77565efb87c8b396
Micah Yong <micahtyong@gmail.com>
2021-03-22 00:13:08 -0700

    Translation and encoding handling function complete

--- src/lib/i18next/i18n.js
+++ src/lib/i18next/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from './resources'; 
 
+// TODO Step 4: Interpret language based on user settings
 i18n.use(initReactI18next).init({
     resources: resources,
     lng: "bur",
--- src/lib/i18next/translator.ts
+++ src/lib/i18next/translator.ts
@@ -1,21 +1,24 @@
-import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+
+/* Encoder between Zawgyi and Unicode */ 
+import { ZawgyiConverter } from './encodings'; 
+const converter = new ZawgyiConverter(); 
 
 enum LanguageEncoding { 
     UNICODE,
     ZAWGYI 
 }
 
-/* Global variable that determines user language */
-let userLanguage = "bur"; 
-let userEncoding: LanguageEncoding = LanguageEncoding.ZAWGYI; 
+/* Global variable that determines user encoding */
+// TODO Step 4: Interpret encoding based on user settings
+let userEncoding: LanguageEncoding = LanguageEncoding.UNICODE; 
 
 export const useIntl = () => {
     const { t } = useTranslation();
     const intl = (text: string) => {
         switch (userEncoding) {
             case LanguageEncoding.ZAWGYI: 
-                return text; 
+                return converter.unicodeToZawgyi(t(text)); 
             default: 
                 return t(text); 
         }
