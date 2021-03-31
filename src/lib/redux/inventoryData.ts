27cffdfdf89c61e56612d4fb7113dd12687d50df
Annie Wang <anniedwang02@gmail.com>
2021-03-30 21:27:57 -0700

    Cleanup, add comment

--- src/lib/redux/inventoryData.ts
+++ src/lib/redux/inventoryData.ts
@@ -1,24 +1,44 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { InventoryRecord, PurchaseRequestRecord, SiteRecord } from '../airtable/interface';
-import { addInventory, addPurchaseRequest, EMPTY_INVENTORY, saveInventoryData, selectCurrentSiteInventoryById, selectProductById, setCurrInventoryId, updateInventoryQuantity, updatePurchaseRequest } from './inventoryDataSlice';
+import {
+  addInventory,
+  addPurchaseRequest,
+  EMPTY_INVENTORY,
+  saveInventoryData,
+  selectCurrentSiteInventoryById,
+  selectProductById,
+  setCurrInventoryId,
+  updateInventoryQuantity,
+  updatePurchaseRequest
+} from './inventoryDataSlice';
 import { getCurrentSite } from './siteData';
 import { RootState, store } from './store';
 
+// Custom selectors for current inventory and product
 export const selectCurrentInventoryId = (state: RootState): string => state.inventoryData.currentInventoryId;
-export const selectCurrentInventory = createSelector(selectCurrentInventoryId, store.getState, (currentInventoryId, state) => selectCurrentSiteInventoryById(state, currentInventoryId));
-export const selectCurrentInventoryProduct = createSelector(selectCurrentInventory, store.getState, (inventory, state) => selectProductById(state, inventory?.productId || ""));
+export const selectCurrentInventory = createSelector(
+  selectCurrentInventoryId,
+  store.getState,
+  (currentInventoryId, state) => selectCurrentSiteInventoryById(state, currentInventoryId),
+);
+export const selectCurrentInventoryProduct = createSelector(
+  selectCurrentInventory,
+  store.getState,
+  (inventory, state) => selectProductById(state, inventory?.productId || ''),
+);
 
-const refreshInventoryData = (site: SiteRecord) : void => {
-  if (site) { // TODO @wangannie what to do if site null
-    const {products, inventory, purchaseRequests, inventoryUpdates } = site;
+const refreshInventoryData = (site: SiteRecord): void => {
+  if (site) {
+    // TODO @wangannie what to do if site null
+    const { products, inventory, purchaseRequests, inventoryUpdates } = site;
     const siteId = site.id;
     const inventoryData = {
       siteId,
       products,
       inventory,
       purchaseRequests,
-      inventoryUpdates
-    }
+      inventoryUpdates,
+    };
     store.dispatch(saveInventoryData(inventoryData));
   }
 };
@@ -30,37 +50,44 @@ const addInventoryToRedux = (inventory: InventoryRecord): void => {
 const addPurchaseRequestToRedux = (purchaseRequest: PurchaseRequestRecord): void => {
   const purchaseRequestData = {
     ...purchaseRequest,
-    siteId: getCurrentSite().id
-  }
+    siteId: getCurrentSite().id,
+  };
   store.dispatch(addPurchaseRequest(purchaseRequestData));
-}
+};
 
 const updatePurchaseRequestInRedux = (purchaseRequest: Partial<PurchaseRequestRecord>): void => {
   const purchaseRequestUpdates = {
     ...purchaseRequest,
-    siteId: getCurrentSite().id
-  }
+    siteId: getCurrentSite().id,
+  };
   store.dispatch(updatePurchaseRequest(purchaseRequestUpdates));
-}
+};
 
-const updateInventoryQuantityInRedux = (inventoryId: string, newQuantity: number) : void => {
+const updateInventoryQuantityInRedux = (inventoryId: string, newQuantity: number): void => {
   const updateData = {
     inventoryId,
     newQuantity,
     siteId: getCurrentSite().id,
-  }
+  };
   store.dispatch(updateInventoryQuantity(updateData));
-}
+};
 
 // Gets the current quantity of an inventory item given its id
 const getInventoryCurrentQuantity = (inventoryId: string): number => {
   const inventory = selectCurrentSiteInventoryById(store.getState(), inventoryId) || EMPTY_INVENTORY;
   return inventory.currentQuantity;
-}
+};
 
 const setCurrentInventoryIdInRedux = (inventoryId: string): void => {
   store.dispatch(setCurrInventoryId(inventoryId));
-}
+};
 
-export { refreshInventoryData, addInventoryToRedux, addPurchaseRequestToRedux, updatePurchaseRequestInRedux, updateInventoryQuantityInRedux, getInventoryCurrentQuantity, setCurrentInventoryIdInRedux };
-
+export {
+  refreshInventoryData,
+  addInventoryToRedux,
+  addPurchaseRequestToRedux,
+  updatePurchaseRequestInRedux,
+  updateInventoryQuantityInRedux,
+  getInventoryCurrentQuantity,
+  setCurrentInventoryIdInRedux,
+};
