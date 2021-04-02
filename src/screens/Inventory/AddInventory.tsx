ee3461075d3bfa369c61a5e3c5796267edc3ac48
Annie Wang <anniedwang02@gmail.com>
2021-04-01 23:08:29 -0700

    Reformat with prettier

--- src/screens/Inventory/AddInventory.tsx
+++ src/screens/Inventory/AddInventory.tsx
@@ -10,7 +10,12 @@ import TextField from '../../components/TextField';
 import { InventoryRecord } from '../../lib/airtable/interface';
 import { createInventory, createProduct } from '../../lib/airtable/request';
 import { addProductToRedux, setCurrentInventoryIdInRedux } from '../../lib/redux/inventoryData';
-import { EMPTY_INVENTORY, EMPTY_PRODUCT, selectAllCurrentSiteInventoryArray, selectAllProducts } from '../../lib/redux/inventoryDataSlice';
+import {
+  EMPTY_INVENTORY,
+  EMPTY_PRODUCT,
+  selectAllCurrentSiteInventoryArray,
+  selectAllProducts
+} from '../../lib/redux/inventoryDataSlice';
 import { getCurrentSiteId } from '../../lib/redux/siteData';
 import { generateOfflineId } from '../../lib/utils/offlineUtils';
 
@@ -23,17 +28,17 @@ const styles = (theme: Theme) =>
       width: '100%',
     },
     newProductContainer: {
-      flexDirection: 'row', 
-      display: 'flex', 
+      flexDirection: 'row',
+      display: 'flex',
       marginBottom: 40,
-    }
+    },
   });
 
 interface AddInventoryProps extends RouteComponentProps {
-  classes: { content: string, formControl: string, newProductContainer: string};
+  classes: { content: string; formControl: string; newProductContainer: string };
 }
 
-function AddInventory (props: AddInventoryProps) {
+function AddInventory(props: AddInventoryProps) {
   const { classes } = props;
   const products = useSelector(selectAllProducts);
   const siteInventory = useSelector(selectAllCurrentSiteInventoryArray);
@@ -42,13 +47,15 @@ function AddInventory (props: AddInventoryProps) {
   // Product IDs for items that the site already has inventory for
   const currentSiteProductIds = siteInventory.map((inventory: InventoryRecord) => inventory.productId);
   // Filter to only show products that the site doesn't already have
-  const productOptionIds = Object.entries(products).filter(([id, _]) => !currentSiteProductIds.includes(id)).map(item => item[0]);
-  
-  const [selectedProductId, setSelectedProductId] = useState("");
+  const productOptionIds = Object.entries(products)
+    .filter(([id, _]) => !currentSiteProductIds.includes(id))
+    .map((item) => item[0]);
+
+  const [selectedProductId, setSelectedProductId] = useState('');
   const [startingAmount, setStartingAmount] = useState(0);
-  const [unit, setUnit] = useState("");
-  const [newProductName, setNewProductName] = useState("");
-  const NEW_PRODUCT_LABEL = "+ New Inventory Item";
+  const [unit, setUnit] = useState('');
+  const [newProductName, setNewProductName] = useState('');
+  const NEW_PRODUCT_LABEL = '+ New Inventory Item';
 
   // TODO: Add form input validation and error messaging
   const handleSubmit = async (event: React.MouseEvent) => {
@@ -65,7 +72,7 @@ function AddInventory (props: AddInventoryProps) {
       try {
         productId = await createProduct(product);
       } catch (error) {
-        console.log("[AddInventory] (createProduct, handleSubmit) Error: ", error);
+        console.log('[AddInventory] (createProduct, handleSubmit) Error: ', error);
         productId = generateOfflineId();
       }
       product.id = productId;
@@ -78,7 +85,7 @@ function AddInventory (props: AddInventoryProps) {
     inventory.siteId = getCurrentSiteId();
     inventory.currentQuantity = startingAmount;
     inventory.periodStartQuantity = startingAmount;
-    
+
     // createInventory returns the inventory item with an id
     inventory = await createInventory(inventory);
 
@@ -87,32 +94,33 @@ function AddInventory (props: AddInventoryProps) {
     // Navigate to new inventory item's profile page
     setCurrentInventoryIdInRedux(inventory.id);
     history.replace(`item`);
-  }
-  const handleSelectItem = (event: React.ChangeEvent<{ }>, value: string | null) => {
-      setSelectedProductId(value || "");
-  }
+  };
+  
+  const handleSelectItem = (event: React.ChangeEvent<{}>, value: string | null) => {
+    setSelectedProductId(value || '');
+  };
 
   const handleStartingAmountInput = (event: React.ChangeEvent<{ value: unknown }>) => {
     setStartingAmount(parseFloat(event.target.value as string) || 0);
-  }
+  };
 
   const handleNewProductName = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setNewProductName(event.target.value as string || "");
-  }
+    setNewProductName((event.target.value as string) || '');
+  };
 
   const handleUnitInput = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setUnit(event.target.value as string || "");
-  }
-  
+    setUnit((event.target.value as string) || '');
+  };
+
   const filter = createFilterOptions<string>();
-  
+
   return (
     <BaseScreen title="New Inventory" leftIcon="backNav">
       <form noValidate className={classes.content} onSubmit={() => false}>
         <FormControl variant="outlined" className={classes.formControl}>
           <Autocomplete
             value={selectedProductId}
-            style = {{ marginBottom: 20 }}
+            style={{ marginBottom: 20 }}
             onChange={handleSelectItem}
             filterOptions={(options, params) => {
               const filtered = filter(options, params);
@@ -121,24 +129,31 @@ function AddInventory (props: AddInventoryProps) {
             }}
             selectOnFocus
             clearOnBlur
-            id = "select-item"
+            id="select-item"
             options={productOptionIds}
-            getOptionLabel={(option) => products[option] ? `${products[option]?.name} (${products[option]?.unit})` : option}
-            renderInput={(params) => <MaterialTextField {...params} label="Item" variant="outlined"/>}
+            getOptionLabel={(option) =>
+              products[option] ? `${products[option]?.name} (${products[option]?.unit})` : option
+            }
+            renderInput={(params) => <MaterialTextField {...params} label="Item" variant="outlined" />}
           />
         </FormControl>
         {/* If the user selected the New Inventory Item option, display extra fields */}
-        {selectedProductId === NEW_PRODUCT_LABEL &&
+        {selectedProductId === NEW_PRODUCT_LABEL && (
           <div className={classes.newProductContainer}>
-            <div style={{marginRight: 10, flex: 2}}>
+            <div style={{ marginRight: 10, flex: 2 }}>
               <TextField label={'New Item'} id={'new-item'} primary={true} onChange={handleNewProductName} />
             </div>
-            <div style={{flex: 1}}>
+            <div style={{ flex: 1 }}>
               <TextField label={'Unit'} id={'unit'} primary={true} onChange={handleUnitInput} />
             </div>
           </div>
-        }
-        <TextField label={'Starting Amount'} id={'starting-amount'} primary={true} onChange={handleStartingAmountInput} />
+        )}
+        <TextField
+          label={'Starting Amount'}
+          id={'starting-amount'}
+          primary={true}
+          onChange={handleStartingAmountInput}
+        />
         <Button label={'Add'} onClick={handleSubmit} />
       </form>
     </BaseScreen>
