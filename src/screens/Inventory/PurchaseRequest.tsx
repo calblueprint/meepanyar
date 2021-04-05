005ba8def9ffae884daf9eb98ae1be3616431557
Annie Wang <wangannie@berkeley.edu>
2021-04-05 10:36:44 -0700

    [Inventory] Integrate camera functionality for purchase request receipts (#69)
    * Working frontend + image loading, jank navigation
    
    * Add receipt uri
    
    * Fix controlledcomponent warning
    
    * Accommodate no photo url
    
    * Add image to review screen
    
    * Small copy updates
    
    * Fix submit navigation with goBack
    
    * Fixed goBack navigation with camera
    
    * Fix navigation pt 2
    
    * Add loading indicator to submit button

--- src/screens/Inventory/PurchaseRequest.tsx
+++ src/screens/Inventory/PurchaseRequest.tsx
@@ -27,10 +27,16 @@ const styles = (theme: Theme) =>
     section: {
       marginTop: '30px',
     },
+    imageContainer: {
+      border: `3.5px solid ${theme.palette.divider}`,
+      radius: '6px',
+      padding: 0,
+      width: '100%',
+    },
   });
 
 interface PurchaseRequestsProps extends RouteComponentProps {
-  classes: { content: string; section: string };
+  classes: { content: string; section: string, imageContainer: string };
   location: any;
 }
 
@@ -57,7 +63,7 @@ function PurchaseRequest(props: PurchaseRequestsProps) {
   };
 
   return (
-    <BaseScreen leftIcon="backNav">
+    <BaseScreen title="Inventory Receipt" leftIcon="backNav">
       <BaseScrollView>
         <div className={classes.content}>
           <Typography variant="h3">{`Purchase Request for ${product.name}`}</Typography>
@@ -67,8 +73,7 @@ function PurchaseRequest(props: PurchaseRequestsProps) {
           <Typography variant="body1">{`Created at ${formatDateStringToLocal(purchaseRequest.createdAt)}`}</Typography>
           <Typography variant="body1">{`Submitted by ${purchaseRequest.requesterId}`}</Typography>
           {purchaseRequest.notes && <Typography variant="body1">{`Notes: ${purchaseRequest.notes}`}</Typography>}
-          {/* TODO: admins lookup user info by id */}
-          {/* TODO: Add image */}
+          {purchaseRequest.receipt && <img className={classes.imageContainer} src={purchaseRequest.receipt[0].url} alt="receipt"/> }
           {purchaseRequest.status == PurchaseRequestStatus.PENDING && (
             <div>
               <Button onClick={() => handleSubmit(purchaseRequest, true)} label={'Approve'} />
