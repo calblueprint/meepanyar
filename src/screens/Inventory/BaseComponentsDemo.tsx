ed183050f81bb4086cff99aaaf23daa7d6efdd4a
Annie Wang <anniedwang02@gmail.com>
2021-04-09 10:53:14 -0700

    Add camera button error state

--- src/screens/Inventory/BaseComponentsDemo.tsx
+++ src/screens/Inventory/BaseComponentsDemo.tsx
@@ -11,10 +11,7 @@ import CameraButton from '../../components/CameraButton';
 import SearchBar from '../../components/SearchBar';
 import TextField from '../../components/TextField';
 import { InventoryRecord } from '../../lib/airtable/interface';
-import {
-  selectAllCurrentSiteInventoryArray,
-  selectAllProducts
-} from '../../lib/redux/inventoryDataSlice';
+import { selectAllCurrentSiteInventoryArray, selectAllProducts } from '../../lib/redux/inventoryDataSlice';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -58,17 +55,24 @@ function BaseComponentsDemo(props: BaseComponentsProps) {
     <BaseScreen title="Base Components" leftIcon="backNav">
       <BaseScrollView>
         <CameraButton id="take-picture" goBack={-1} label="Camera button label" />
-        <CameraButton id="picture-taken" photoUri="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/ReceiptSwiss.jpg/170px-ReceiptSwiss.jpg" goBack={-1} label="Camera button with image" />
+        <CameraButton id="take-picture-error" goBack={-1} label="Camera button with error" error helperText="Could not add photo. Please try again." />
         
+        <CameraButton
+          id="picture-taken"
+          photoUri="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/ReceiptSwiss.jpg/170px-ReceiptSwiss.jpg"
+          goBack={-1}
+          label="Camera button with image"
+        />
+
         <SearchBar onSearchChange={() => null} placeholder="Search for..." />
         <Button label={'Default'} />
-        <Button label={'Default with startIcon'} startIcon={<DeleteIcon/>}/>
+        <Button label={'Default with startIcon'} startIcon={<DeleteIcon />} />
         <Button label={'Default Full Width'} fullWidth />
         <Button label={'Outlined'} variant="outlined" />
-        <Button loading={loading} onClick={() => setLoading(!loading)} label={'Click to initiate loading'}/>
+        <Button loading={loading} onClick={() => setLoading(!loading)} label={'Click to initiate loading'} />
         <Button label={'Disabled'} disabled />
         <Button label={'Text Button'} variant="text" />
-        <Button label={'Text Button Disabled'} variant="text" disabled/>
+        <Button label={'Text Button Disabled'} variant="text" disabled />
 
         <form noValidate className={classes.content} onSubmit={() => false}>
           <FormControl variant="outlined" className={classes.formControl}>
@@ -94,46 +98,27 @@ function BaseComponentsDemo(props: BaseComponentsProps) {
               <TextField label={'Unit'} id={'unit'} />
             </div>
           </div>
-          <TextField
-            disabled
-            label={'Disabled'}
-            id={'disabled'}
-            placeholder={"Disabled placeholder"}
-          />
-          <TextField
-            label={'Resting Required with Helper Text'}
-            id={'resting-required'}
-            helperText={"Helper text"}
-          />
-          <TextField
-            label={'Resting'}
-            id={'resting'}
-            placeholder={"Resting Placeholder"}
-          />
+          <TextField disabled label={'Disabled'} id={'disabled'} placeholder={'Disabled placeholder'} />
+          <TextField label={'Resting Required with Helper Text'} id={'resting-required'} helperText={'Helper text'} />
+          <TextField label={'Resting'} id={'resting'} placeholder={'Resting Placeholder'} />
 
           <TextField
             label={'Error'}
             id={'error'}
             error
-            placeholder={"Error Placeholder"}
-            helperText={"Error message"}
+            placeholder={'Error Placeholder'}
+            helperText={'Error message'}
           />
 
           <TextField
-            label='Numbers only'
-            id='numbers-only'
-            type='number'
+            label="Numbers only"
+            id="numbers-only"
+            type="number"
             placeholder="Placeholder"
             helperText="Only numbers!"
           />
-          
-          <TextField
-            label={'With Unit'}
-            id={'with-unit'}
-            placeholder={"33.33"}
-            unit={'bottle'}
-          />
-          
+
+          <TextField label={'With Unit'} id={'with-unit'} placeholder={'33.33'} unit={'bottle'} />
         </form>
       </BaseScrollView>
     </BaseScreen>
