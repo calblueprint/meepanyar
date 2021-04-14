1e1586694b0fda30639f933acb815c580c019431
cnnmon <cnnmonn@gmail.com>
2021-04-13 19:28:02 -0700

    Tiffany/profile frontend (#70)
    * profile
    
    * Update CustomerProfile.tsx
    
    * Update CustomerProfile.tsx
    
    * Update CustomerProfile.tsx
    
    * pr review
    
    * freeunits
    
    * pr review
    
    * pr review
    
    * pr review
    
    * base components
    
    * pr review
    
    * Delete TariffPlanInfo.tsx
    
    * Update CustomerProfile.tsx
    
    * pr review
    
    * pr review
    
    * outlinedcardlist fix

--- src/components/OutlinedCardList.tsx
+++ src/components/OutlinedCardList.tsx
@@ -1,5 +1,8 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import CreateIcon from '@material-ui/icons/Create';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 export interface CardPropsInfo {
@@ -10,69 +13,102 @@ export interface CardPropsInfo {
 
 interface CardProps {
   info: CardPropsInfo[];
-  primary: boolean;
+  highlighted?: boolean;
   rightIcon?: JSX.Element;
   columns?: boolean;
+  readOnly?: boolean;
+  editPath?: string;
+  reverse?: boolean;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    root: (props: CardProps) => ({
       marginTop: '10px',
       border: '1px solid',
-      borderColor: theme.palette.divider,
+      borderColor: props.readOnly ? theme.palette.background.default : theme.palette.text.disabled,
       borderRadius: '6px',
-    },
+      backgroundColor: props.readOnly ? theme.palette.background.default : 'white'
+    }),
     content: (props: CardProps) => ({
       flex: 1,
       padding: '8px 8px',
       display: 'flex',
-      flexDirection: props.columns ? 'row' : 'column'
+      flexDirection: props.columns ? 'row' : 'column',
     }),
     itemWrapper: {
       flex: 1,
       display: 'flex',
       justifyContent: 'space-between',
       alignItems: 'center',
+      margin: '5px',
     },
     items: (props: CardProps) => ({
       flex: 1,
       flexDirection: props.columns ? 'column-reverse' : 'column',
       display: 'flex',
-      color: theme.palette.text.primary,
-      margin: '5px',
     }),
     fullItems: {
       width: '100%',
       color: theme.palette.text.primary,
-      margin: '5px',
+    },
+    reverseLabel: {
+      wordSpacing: '100vw',
+      lineHeight: '1.2',
+    },
+    editButton: {
+      float: 'right',
+    },
+    editIcon: {
+      color: theme.palette.primary.main,
+      fontSize: '14px',
+      marginLeft: '5px',
     },
   }),
 );
 
-
 export default function OutlinedCardList(props: CardProps): JSX.Element {
   const classes = useStyles(props);
+
+  const getEditButton = (pathname: string) => {
+    return (
+      <IconButton
+        component={Link}
+        to={{ pathname }}
+        size="small"
+        className={classes.editButton}
+      >
+        <CreateIcon className={classes.editIcon} />
+      </IconButton>
+    );
+  }
 
   const getLabeledNumber = (
     key: number,
     number: string,
     label: string,
     unit: string,
-    primary: boolean,
-    rightIcon: JSX.Element | null,
-    columns: boolean,
   ) => {
+    const { columns, rightIcon, editPath, highlighted } = props;
+    const getFormattedLabel = () => (
+      <div>
+        {editPath ? getEditButton(editPath) : null}
+        <Typography align={columns ? 'center' : 'inherit'} className={props.reverse ? classes.reverseLabel : undefined}>{label}</Typography>
+      </div>
+    );
+    const getFormattedNumber = () => (
+      <Typography variant="h3" align={columns ? 'center' : 'inherit'} color={highlighted ? 'error' : 'inherit'}>
+        {number} {unit}
+      </Typography>
+    );
     return (
       <div key={key} className={ classes.itemWrapper }>
         <div className={rightIcon ? classes.items : classes.fullItems}>
-          <Typography variant="body1" align={columns ? 'center' : 'inherit'}>{label}</Typography>
-          <Typography variant="h3" align={columns ? 'center' : 'inherit'} color={primary ? 'primary' : 'inherit'}>
-            {number} {props.columns ? '' : unit}
-          </Typography>
+          {props.reverse ? getFormattedNumber() : getFormattedLabel()}
+          {props.reverse ? getFormattedLabel() : getFormattedNumber()}
           {/* Split unit to new line if in column layout */}
-          {props.columns && 
-            <Typography variant="h3" align={columns ? 'center' : 'inherit'} color={primary ? 'primary' : 'inherit'}>
+          {!props.reverse && columns &&
+            <Typography variant="h3" align={columns ? 'center' : 'inherit'} color={highlighted ? 'primary' : 'inherit'}>
               {unit}
             </Typography>
           }
@@ -82,14 +118,11 @@ export default function OutlinedCardList(props: CardProps): JSX.Element {
     );
   };
 
-  const rightIcon = props.rightIcon || null;
-  const columns = props.columns || false;
-
   return (
     <div className={classes.root}>
       <div className={classes.content}>
         {props.info.map((info, index) =>
-          getLabeledNumber(index, info.number, info.label, info.unit, props.primary, rightIcon, columns),
+          getLabeledNumber(index, info.number, info.label, info.unit),
         )}
       </div>
     </div>
@@ -99,4 +132,5 @@ export default function OutlinedCardList(props: CardProps): JSX.Element {
 OutlinedCardList.defaultProps = {
   rightIcon: null,
   columns: false,
+  highlighted: false,
 };
