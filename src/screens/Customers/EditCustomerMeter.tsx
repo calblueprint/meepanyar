2233b58429ea74084ab2b9b7d5b8784827b7708a
Julian Kung <julianrkung@berkeley.edu>
2021-04-19 22:48:55 -0700

    EditCustomerMeter validation done

--- src/screens/Customers/EditCustomerMeter.tsx
+++ src/screens/Customers/EditCustomerMeter.tsx
@@ -7,69 +7,94 @@ import TextField from '../../components/TextField';
 import { MeterType } from '../../lib/redux/customerDataSlice';
 import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import Button from '../../components/Button';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export interface EditCustomerMeterState {
     meterType: string | undefined;
     meterNumber: string | undefined;
 }
 
+const validationSchema = yup.object({
+    meterType: yup.string().oneOf([MeterType.NO_METER, MeterType.SMART_METER, MeterType.ANALOG_METER], 'Meter Type must be one of Smart, Analog, or No Meter'),
+    meterNumber: yup.mixed() // Can be number or empty string
+        .when('meterType', {
+            is: (MeterType.ANALOG_METER || MeterType.SMART_METER),
+            then: yup.mixed().required('Please enter a meter number'),
+        })
+});
+
 function EditCustomerMeter() {
+
     const currentCustomer = useSelector(selectCurrentCustomer);
-    const [meterType, setMeterType] = useState<EditCustomerMeterState["meterType"]>(currentCustomer?.meterType);
-    const [meterNumber, setMeterNumber] = useState<EditCustomerMeterState["meterNumber"]>(currentCustomer?.meterNumber.toString());
+
+    const formik = useFormik({
+        initialValues: {
+            meterType: currentCustomer?.meterType,
+            meterNumber: currentCustomer?.meterNumber.toString()
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            handleSubmit(values);
+        },
+    })
     const history = useHistory();
 
     if (!currentCustomer) {
         return <Redirect to='/customers' />
     }
 
-    const handleSelectMeterType = (event: React.ChangeEvent<{ value: unknown }>) => {
-        const selectedMeterType: MeterType = event.target.value as MeterType;
-        setMeterType(selectedMeterType);
-
-        if (selectedMeterType === MeterType.NO_METER) {
-            setMeterNumber('')
-        }
-    }
-
-    const handleMeterNumberInput = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setMeterNumber(event.target.value as string);
-    }
-
-    const handleSubmit = (event: React.MouseEvent) => {
-        event.preventDefault();
-
-        // TODO: Make sure you can't submit if no meter number and the meter type isn't NO_METER
+    const handleSubmit = (values: any) => {
+        const { meterNumber, meterType } = values;
         history.push('tariff-plans', { meterNumber, meterType })
     }
 
     return (
         <BaseScreen title="Edit Customer" leftIcon="backNav">
-            <FormControl
-                required
-                variant="outlined"
-                style={{ width: '100%' }}
-            >
-                <InputLabel>Meter Type</InputLabel>
-                <Select onChange={handleSelectMeterType} label={'Meter Type'} value={meterType}>
-                    <MenuItem hidden value={MeterType.INACTIVE}>Inactive</MenuItem>
-                    <MenuItem value={MeterType.ANALOG_METER}>Analog Meter</MenuItem>
-                    <MenuItem value={MeterType.SMART_METER}>Smart Meter</MenuItem>
-                    <MenuItem value={MeterType.NO_METER}>No Meter</MenuItem>
-                </Select>
-            </FormControl>
+            <form noValidate onSubmit={formik.handleSubmit}>
 
-            <TextField
-                label='Meter Number'
-                id={'meter-number'}
-                placeholder='Input'
-                value={meterNumber}
-                disabled={meterType === MeterType.INACTIVE || meterType === MeterType.NO_METER}
-                required={meterType !== MeterType.INACTIVE && meterType !== MeterType.NO_METER}
-                onChange={handleMeterNumberInput}
-            />
+                <FormControl
+                    required
+                    variant="outlined"
+                    style={{ width: '100%' }}
+                >
+                    <InputLabel>Meter Type</InputLabel>
+                    <Select
+                        onChange={(event) => {
+                            const meterType = event.target.value as string;
+                            formik.setFieldValue('meterType', meterType)
 
-            <Button label='Next' onClick={handleSubmit} />
+                            // Clear the meterNumber if NO_METER is selected
+                            if (meterType === MeterType.NO_METER) {
+                                formik.setFieldValue('meterNumber', '')
+                            }
+                        }}
+                        label={'Meter Type'}
+                        id='meterType'
+                        required
+                        value={formik.values.meterType}
+                    >
+                        <MenuItem hidden value={MeterType.INACTIVE}>Inactive</MenuItem>
+                        <MenuItem value={MeterType.ANALOG_METER}>Analog Meter</MenuItem>
+                        <MenuItem value={MeterType.SMART_METER}>Smart Meter</MenuItem>
+                        <MenuItem value={MeterType.NO_METER}>No Meter</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <TextField
+                    label='Meter Number'
+                    id={'meterNumber'}
+                    placeholder='Input'
+                    value={formik.values.meterNumber}
+                    disabled={formik.values.meterType === MeterType.INACTIVE || formik.values.meterType === MeterType.NO_METER}
+                    required={formik.values.meterType !== MeterType.INACTIVE && formik.values.meterType !== MeterType.NO_METER}
+                    error={formik.touched.meterNumber && Boolean(formik.errors.meterNumber)}
+                    helperText={formik.touched.meterNumber && formik.errors.meterNumber}
+                    onChange={formik.handleChange}
+                />
+
+                <Button label='Next' />
+            </form>
         </BaseScreen>
     );
 }
