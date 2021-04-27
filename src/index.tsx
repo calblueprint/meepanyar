df0b828cff636adb57796a1e4a00ef9e3f385d6b
Julian Kung <julianrkung@berkeley.edu>
2021-04-26 22:10:14 -0700

    Offline refresh via channels

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,19 @@ setInterval(checkOnline, 10000);
 //refreshes data in the background every 15 min
 setInterval(refreshDataBackground, 900000);
 
+// Set up broadcast channel between the client and service worker
+// this channel is used by the service worker to tell the client 
+// to refresh site data once all requests have been sent.
+const postQueueMonitorChannel = new BroadcastChannel('refresh-data-channel');
+
+postQueueMonitorChannel.onmessage = (event) => {
+  if (event.data && event.data.replayQueueLength === 0) {
+    console.log("Offline requests finished sending. Refreshing data...")
+    refreshDataBackground().then(() => console.log("...Finished refreshing data"))
+  }
+}
+
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
