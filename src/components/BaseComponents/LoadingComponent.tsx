d59c30c0c1cfe22b0924fe72bf1c356cc87173e2
Julian Kung <julianrkung@berkeley.edu>
2021-04-29 22:36:56 -0700

    login won't get stuck in loading

--- src/components/BaseComponents/LoadingComponent.tsx
+++ src/components/BaseComponents/LoadingComponent.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withStyles, createStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router';
 
 const styles = () =>
   createStyles({
@@ -19,6 +20,17 @@ interface LoadingComponentProps {
 
 const LoadingComponent = (props: LoadingComponentProps) => {
   const { classes } = props;
+  const history = useHistory();
+
+  // If a user is stuck on the loading screen for more than 15 seconds, we send them back to the login screen
+  useEffect(() => {
+    const timeoutId = setTimeout(() => history.push('/login'), 15000)
+    return () => {
+      clearTimeout(timeoutId)
+      console.log("Timeout cleared")
+    }
+  })
+
   return (
     <div className={classes.loadingComponent}>
       <h1> Loading ... </h1>
